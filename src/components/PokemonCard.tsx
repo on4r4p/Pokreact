856927a9b9227266b32f7c1aec458343aa980f41
on4r4p <on4r4p@disroot.org>
2025-12-03 12:12:21 +0100

    Miu miu

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,33 @@
+
+
+const pokemonList = [
+
+    {
+
+        name: "bulbizar",
+
+        imgSrc:
+
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+    },
+
+    {
+
+        name: "mew",
+        imgSrc:
+
+            "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/151.png"
+    },
+
+];
+
 export default function PokemonCard() {
+    const pokemon = pokemonList[1]
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pikachu" />
-            <figcaption>Bulbizar</figcaption>
+            <img src={pokemon.imgSrc} alt={pokemon.name} />
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
